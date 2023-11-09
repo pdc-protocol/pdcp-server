9d98f9d35eeddfdf7578e232b91e497efdc5f83c
Savo SAICIC <ssaicic@corp.free.fr>
2023-11-09 17:21:38 +0100

    feat: add new projects module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [ProjectsModule],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/projects/projects.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+
+import { ProjectsService } from './projects.service';
+
+@Controller('projects')
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+}
