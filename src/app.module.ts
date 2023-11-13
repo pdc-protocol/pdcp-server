5430ce2f580d8e23908d3afe4264752e68ed3d75
Savo SAICIC <ssaicic@corp.free.fr>
2023-11-13 12:12:04 +0100

    feat: add a /health route with terminus

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [HealthModule],
   providers: [AppService],
 })
 export class AppModule {}
