8406692a07fdb55c84fed07488b11f4c7cfb2279
Savo SAICIC <ssaicic@corp.free.fr>
2023-11-13 14:36:44 +0100

    feat: add a /health/readiness route

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,13 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(private health: HealthCheckService) {}
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([]);
+  }
+
+  @Get('readiness')
+  @HealthCheck()
+  checkReadiness() {
+    return this.health.check([
+      () => this.http.pingCheck('http check', 'http://localhost:3000/health'),
+    ]);
   }
 }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule],
   controllers: [HealthController],
+  imports: [TerminusModule, HttpModule],
 })
 export class HealthModule {}
